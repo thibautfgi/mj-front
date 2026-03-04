8a16b3571b7ccd50148e6f39a8a315374b7b3ea3
ThibautFgi <thibaut.fgi@hotmail.com>
2026-03-04 10:44:11 +0100

    env

--- src/app/components/page/test/test.component.ts
+++ src/app/components/page/test/test.component.ts
@@ -14,7 +14,7 @@ export class TestComponent implements OnInit, OnDestroy {
   private mapContainer = viewChild.required<ElementRef<HTMLDivElement>>('mapContainer');
 
   ngOnInit(): void {
-    (mapboxgl as any).accessToken = environment.MAPBOX_TOKEN_PUBLIC
+    (mapboxgl as any).accessToken = environment.MAPBOX_TO KEN_PUBLIC
 
     const mapInstance = new mapboxgl.Map({
       container: this.mapContainer().nativeElement,
