7a17f2b2e0b382b779453e05b969f49f523487d2
ThibautFgi <thibaut.fgi@hotmail.com>
2026-05-30 19:11:53 +0200

    ajout des thuiles de neige

--- environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  authApiUrl: 'http://localhost:8081',
-  moteurApiUrl: 'http://localhost:8080',
-  MAPBOX_TOKEN_PUBLIC: '',
-  copernicusInstanceId: '',
-  snowLayerId: ''
-};
